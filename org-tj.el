3ffced88f25a03595203fccf35f2bf80c0e35c02
ndwarshuis <natedwarshuis@gmail.com>
2019-05-01 18:21:16 -0400

    renamed the function base name

--- org-tj.el
+++ org-tj.el
@@ -39,7 +39,7 @@
 (require 'helm)
 (require 'ox-taskjuggler)
 
-(defmacro org-tj3--with-advice (adlist &rest body)
+(defmacro org-tj--with-advice (adlist &rest body)
   "Execute BODY with temporary advice in ADLIST.
 
 Each element of ADLIST should be a list of the form
@@ -63,7 +63,7 @@ event of an error or nonlocal exit."
   :tag "Org TJ3"
   :group 'org)
 
-(defcustom org-tj3-config-file (-> "XDG_CONFIG_HOME"
+(defcustom org-tj-config-file (-> "XDG_CONFIG_HOME"
                                    getenv
                                    file-name-as-directory
                                    (concat "tj3")
@@ -73,12 +73,12 @@ event of an error or nonlocal exit."
   :group 'org-tj3
   :type 'string)
 
-(defcustom org-tj3-default-attributes
+(defcustom org-tj-default-attributes
   '(("timingresolution" . "5 min"))
   "Default project attributes."
   :group 'org-tj3)
 
-(defun org-tj3--cmd (type &rest args)
+(defun org-tj--cmd (type &rest args)
   "Return formatted shell command string for TYPE.
 ARGS are strings appended to the end of the command."
   (--> 
@@ -90,21 +90,21 @@ ARGS are strings appended to the end of the command."
    (list it)
    (append it (list "--silent" "--no-color"))
    (append it args)
-   (if (not org-tj3-config-file) it
-     (-insert-at 1 (concat "-c " org-tj3-config-file) it))
+   (if (not org-tj-config-file) it
+     (-insert-at 1 (concat "-c " org-tj-config-file) it))
    (s-join " " it)))
 
-(defun org-tj3--compile-no-report (orig-fn file)
+(defun org-tj--compile-no-report (orig-fn file)
   "Process FILE using ORIG-FN but don't compile to a report.
 Instead override the `org-taskjuggler-process-command' such that FILE
 added to the web server, and don't make the report directory as it is
 not necessary."
   (let ((file (expand-file-name file)))
-    (org-tj3--with-advice
+    (org-tj--with-advice
         ;; don't make a reports directory
         ((#'make-directory :override #'ignore))
       ;; override the original process command to load into server
-      (let ((org-taskjuggler-process-command (org-tj3--cmd 'client "add" "%f")))
+      (let ((org-taskjuggler-process-command (org-tj--cmd 'client "add" "%f")))
         (funcall orig-fn file)))
     ;; just return the file path when done
     ;; the list is necessary for org-taskjuggler-export-process-and-open
@@ -112,29 +112,29 @@ not necessary."
     ;; expects a list
     (list file)))
 
-(defun org-tj3-open-id-in-browser (id)
+(defun org-tj-open-id-in-browser (id)
   "Open project ID in the default web browser."
   (browse-url
    (format
     ;; TODO make port a variable
     "http://localhost:8081/taskjuggler?project=%s;report=report" id)))
 
-(defun org-tj3-open-in-browser (file)
+(defun org-tj-open-in-browser (file)
   "Open tj3 FILE in the default web browser."
   ;; TODO check if project is loaded and load if not loaded
   ;; if it is loaded throw a warning
   (--> (with-temp-buffer (insert-file-contents file) (buffer-string))
        (s-match "project \\([^[:space:]]+\\) " it)
        (nth 1 it)
-       (org-tj3-open-id-in-browser it)))
+       (org-tj-open-id-in-browser it)))
        
-(defun org-tj3--export-process-and-open-web (orig-fun &rest args)
+(defun org-tj--export-process-and-open-web (orig-fun &rest args)
   "Process ARGS using ORIG-FUN but open files in browser."
-  (org-tj3--with-advice
-      ((#'org-open-file :override #'org-tj3-open-in-browser))
+  (org-tj--with-advice
+      ((#'org-open-file :override #'org-tj-open-in-browser))
     (apply orig-fun args)))
 
-(defun org-tj3--add-attributes (orig-fun &rest args)
+(defun org-tj--add-attributes (orig-fun &rest args)
   "Call ORIG-FUN with ARGS and add extra attributes to projects."
   ;; assume the original list is a newline-delimited string
   ;; break this string into cons cells of keyval pairs
@@ -144,39 +144,39 @@ not necessary."
                (--map (s-split-up-to " " it 1 t) it)
                (--map (cons (car it) (cadr it)) it)))
          (add-attributes
-          (--> org-tj3-default-attributes
+          (--> org-tj-default-attributes
                (--remove (assoc-string (car it) orig-attributes) it))))
     (--> orig-attributes
          (append it add-attributes)
          (--map (format "%s %s\n" (car it) (cdr it)) it)
          (string-join it))))
 
-(defun org-tj3--get-project-id (project info)
+(defun org-tj--get-project-id (project info)
   "Get the project id from PROJECT.
 The id is just the 'TASK_ID' org property with 'prj_' appended.
 INFO is a communication channel and is ignored"
   (-some->> (org-element-property :TASK_ID project)
             (format "prj_%s")))
 
-(defun org-tj3--add-project-attributes (orig-fun project info)
+(defun org-tj--add-project-attributes (orig-fun project info)
   "Call ORIG-FUN with PROJECT and INFO.
 Add project attributes to PROJECT and also add the project id."
-  (org-tj3--with-advice
+  (org-tj--with-advice
       ;; add default attributes
       ((#'org-taskjuggler--build-attributes
-        :around #'org-tj3--add-attributes)
+        :around #'org-tj--add-attributes)
        ;; add the project id
        ;; just use the toplevel id and add "prj_" to the front
        (#'org-taskjuggler-get-id
-        :override #'org-tj3--get-project-id))
+        :override #'org-tj--get-project-id))
     (funcall orig-fun project info)))
 
-(defun org-tj3-add-to-server (file)
+(defun org-tj-add-to-server (file)
   "Add tj3 project FILE to web server."
   (call-process-shell-command
    (format "tj3client -c /home/ndwar/.config/tj3/taskjugglerrc add %s" file)))
 
-(defun org-tj3-load ()
+(defun org-tj-load ()
   "Load tj3 files to the taskjuggler daemon."
   ;; TODO browse folders freely like dired
   (interactive)
@@ -192,11 +192,11 @@ Add project attributes to PROJECT and also add the project id."
         :candidates project-files
         :action
         ;; TODO add load and view using function below
-        '(("Load" . org-tj3-add-to-server))))
+        '(("Load" . org-tj-add-to-server))))
      :buffer "*helm taskjuggler buffer*"
      :prompt "Project: ")))
 
-(defun org-tj3-loaded-ids ()
+(defun org-tj-loaded-ids ()
   "Get a list of loaded project id's from the taskjuggler daemon."
   (-->
    ;; assume --silent and /dev/null remove extraneous info
@@ -207,16 +207,16 @@ Add project attributes to PROJECT and also add the project id."
    (-drop 2 it)
    (--map (--> (s-split "|" it t) (nth 1 it) (s-trim it)) it)))
 
-(defun org-tj3-loaded-projects ()
+(defun org-tj-loaded-projects ()
   "Browse a loaded taskjuggler projects using helm."
   (interactive)
   (helm
    :sources
    (list
     (helm-build-sync-source "Loaded Projects"
-      :candidates (org-tj3-loaded-ids)
+      :candidates (org-tj-loaded-ids)
       :action
-      '(("Open" . org-tj3-open-id-in-browser)
+      '(("Open" . org-tj-open-id-in-browser)
         ("Remove" . 
          (lambda (i)
            (call-process-shell-command
@@ -226,11 +226,11 @@ Add project attributes to PROJECT and also add the project id."
    :prompt "Project ID: "))
 
 (advice-add #'org-taskjuggler--build-project :around
-            #'org-tj3--add-project-attributes)
+            #'org-tj--add-project-attributes)
 (advice-add #'org-taskjuggler-export-process-and-open :around
-            #'org-tj3--export-process-and-open-web)
+            #'org-tj--export-process-and-open-web)
 (advice-add #'org-taskjuggler-compile :around
-            #'org-tj3--compile-no-report)
+            #'org-tj--compile-no-report)
 
 (provide 'org-tj)
 ;;; org-tj.el ends here
