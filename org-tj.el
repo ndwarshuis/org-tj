2a8ad935b73a55f755797b83526f34a99b8c074d
ndwarshuis <natedwarshuis@gmail.com>
2019-06-16 20:09:59 -0400

    REF remove dead macro code

--- org-tj.el
+++ org-tj.el
@@ -240,27 +240,6 @@ task buckets, while still sharing the same resources pool."
   "Hook run after a TaskJuggler files has been saved.
 This hook is run with the name of the file as argument.")
 
-;;; macros
-
-;; (defmacro org-tj--with-advice (adlist &rest body)
-;;   "Execute BODY with temporary advice in ADLIST.
-
-;; Each element of ADLIST should be a list of the form
-;;   (SYMBOL WHERE FUNCTION [PROPS])
-;; suitable for passing to `advice-add'.  The BODY is wrapped in an
-;; `unwind-protect' form, so the advice will be removed even in the
-;; event of an error or nonlocal exit."
-;;   (declare (debug ((&rest (&rest form)) body))
-;;            (indent 1))
-;;   `(progn
-;;      ,@(mapcar (lambda (adform)
-;;                  (cons 'advice-add adform))
-;;                adlist)
-;;      (unwind-protect (progn ,@body)
-;;        ,@(mapcar (lambda (adform)
-;;                    `(advice-remove ,(car adform) ,(nth 2 adform)))
-;;                  adlist))))
-
 ;;; Internal Functions
 
 (defun org-tj--indent-string (s)
